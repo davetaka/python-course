d39ef5b4c7597887df7cf996482cd74f58d803d1
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-01 22:02:37 -0300

    add for loop lessons

---
+++ content/18-for-exercise.py
@@ -0,0 +1,9 @@
+prices = [10,20,30]
+
+total = 0
+
+for price in prices:
+    print(f"price: {price}")
+    total += price
+
+print(f"Total {total}")
---
+++ content/18-for-loops.py
@@ -0,0 +1,6 @@
+for item in ["Mosh", "John"]:
+    print(item)
+
+for item in range(5, 10, 2):
+    print(item)
+
