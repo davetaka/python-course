b0348995a2aea1cc2ea166e01d17e8f9d64cfb4d
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-07 19:43:37 -0300

    add emoji converter exercise

---
+++ content/26-emoji-converter.py
@@ -0,0 +1,14 @@
+message = input("> ")
+
+words = message.split(" ")
+
+emojis = {
+    ":)": "smile",
+    ":(": "sad"
+}
+
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+
+print(output)
