824cd0254fb006605c498fc67894e477d76d72ad
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-10 01:11:43 -0300

    add classes lessons

---
+++ content/34-classes.py
@@ -0,0 +1,21 @@
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 100
+point1.draw()
+
+print(point1.x)
+
+
+point2 = Point()
+point2.draw()
+
+# this gives an error
+# print(point2.x)
