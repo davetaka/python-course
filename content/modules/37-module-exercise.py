a328760b5acaaf595bd0b73f8c21684a7a59481e
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-10 01:12:01 -0300

    add modules lessons and exercises

---
+++ content/modules/37-module-exercise.py
@@ -0,0 +1,5 @@
+from utils import find_max
+
+numbers = [10, 54, 312, 54, 1, 8]
+
+print(find_max(numbers))
---
+++ content/modules/37-modules.py
@@ -0,0 +1,6 @@
+import converters
+# or
+from converters import kg_to_lbs
+
+weight = converters.kg_to_lbs(10)
+print(weight)
