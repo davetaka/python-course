88f7234756d84af9e0f7978a37a8b4534e629a16
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-13 16:51:55 -0300

    add random lesson

---
+++ content/basics-lessons/39-generate-random-exercise.py
@@ -0,0 +1,25 @@
+import random
+
+class Dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6) 
+        return first, second
+
+
+dice = Dice()
+print(dice.roll())
+
+
+# my suggestion
+
+class MyDice:
+    def roll(self):
+        return random.randint(1, 6)
+
+print("My suggestion")
+
+dice1 = MyDice()
+dice2 = MyDice()
+
+print(f"({dice1.roll()}, {dice2.roll()})")
